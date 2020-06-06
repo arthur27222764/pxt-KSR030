a0b89760a47285436cf1c2f9cb18bf23aed41279
kaise-ksrobot <arthur@kaise.com.tw>
2020-06-06 15:34:56 +0800

    v0.6.4

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  */
 //% weight=10 color=#00A6F0 icon="\uf085" block="KSR030"
 namespace KSR030 {
-    
+
     const SERVOMIN = 104 // this is the 'minimum' pulse length count (out of 4096)
     const SERVOMAX = 510 // this is the 'maximum' pulse length count (out of 4096)
     const IIC_ADDRESS = 0x40
@@ -11,7 +11,7 @@ namespace KSR030 {
     const PRESCALE = 0xFE
     const LED0_ON_L = 0x06
 
-    
+
     export enum ServoNum {
         S0 = 0,
         S1 = 1,
@@ -25,32 +25,32 @@ namespace KSR030 {
         S9 = 9,
         S10 = 10,
         S11 = 11,
-     }
+    }
     export enum MotorNum {
         //% blockId="M1A" block="Right"
         M1A = 0,
         //% blockId="M1B" block="Left"
-		M1B = 1,
+        M1B = 1,
 
     }
     export enum LedNum {
         //% blockId="Left_LED" block="Left"
         L_LED = 0,
         //% blockId="Right_LED" block="Right"
-		R_LED = 1,
+        R_LED = 1,
 
     }
-    export enum Track{
+    export enum Track {
         //% blockId="TrackLeft" block="Left"
-        Left=0,
+        Left = 0,
         //% blockId="TrackRight" block="Right"
-        Right=1,
+        Right = 1,
     }
 
     export enum RunState {
         //% blockId="Go_Forward" block="Forward"
         Forward = 0,
-        //% blockId="Car_Back" block="Back"
+        //% blockId="Car_Back" block="Backward"
         Back = 1,
         //% blockId="Go_Left" block="Left"
         Left = 2,
@@ -58,7 +58,7 @@ namespace KSR030 {
         Right = 3,
         //% blockId="Go_Stop" block="Stop"
         Stop = 4,
-     
+
     }
     export enum FrqState {
         //% blockId="Frq_A" block="A"
@@ -73,7 +73,7 @@ namespace KSR030 {
         E = 4,
         //% blockId="Frq_F" block="F"
         F = 5,
-     
+
     }
 
 
@@ -81,17 +81,17 @@ namespace KSR030 {
 
     let initialized = false;
     let neoStrip: neopixel.Strip;
-	
+
     function i2c_write(reg: number, value: number) {
-        
+
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
         pins.i2cWriteBuffer(IIC_ADDRESS, buf)
     }
 
-    function i2c_read(reg: number){
-        
+    function i2c_read(reg: number) {
+
         pins.i2cWriteNumber(IIC_ADDRESS, reg, NumberFormat.UInt8BE);
         let val = pins.i2cReadNumber(IIC_ADDRESS, NumberFormat.UInt8BE);
         return val;
@@ -102,91 +102,115 @@ namespace KSR030 {
         pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
         i2c_write(MODE1, 0x00);
         // Constrain the frequency
-        setFreq(50);
+
+        switch (DETECT_Frequency(ServoNum.S0, DigitalPin.P2)) {
+            case 65:
+                Frq_Set(FrqState.A)
+                break;
+            case 66:
+                Frq_Set(FrqState.B)
+                break;
+            case 67:
+                Frq_Set(FrqState.C)
+                break;
+            case 68:
+                Frq_Set(FrqState.D)
+                break;
+            case 69:
+                Frq_Set(FrqState.E)
+                break;
+            case 70:
+                Frq_Set(FrqState.F)
+                break;
+            default:
+                setFreq(50);
+                break;
+
+
+        }
+
         initialized = true;
     }
 
     function setFreq(freq: number): void {
-        let prescaleval = 25000000/4096/freq;
+        let prescaleval = 25000000 / 4096 / freq;
         prescaleval -= 1;
-        let prescale = prescaleval; 
+        let prescale = prescaleval;
         //let prescale = 121;
-        let oldmode = i2c_read(MODE1);        
+        let oldmode = i2c_read(MODE1);
         let newmode = (oldmode & 0x7F) | 0x10; // sleep
         i2c_write(MODE1, newmode); // go to sleep
         i2c_write(PRESCALE, prescale); // set the prescaler
         i2c_write(MODE1, oldmode);
         control.waitMicros(5000);
         i2c_write(MODE1, oldmode | 0xa0);
-	}
-	
-	function setPwm(channel: number, on: number, off: number): void {
-		if (channel < 0 || channel > 15)
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
             return;
 
         let buf = pins.createBuffer(5);
         buf[0] = LED0_ON_L + 4 * channel;
         buf[1] = on & 0xff;
-        buf[2] = (on>>8) & 0xff;
+        buf[2] = (on >> 8) & 0xff;
         buf[3] = off & 0xff;
-        buf[4] = (off>>8) & 0xff;
+        buf[4] = (off >> 8) & 0xff;
         pins.i2cWriteBuffer(IIC_ADDRESS, buf);
-	}	
+    }
 
-	function servo_map(x: number, in_min: number, in_max: number, out_min: number, out_max: number)
-    {
+    function servo_map(x: number, in_min: number, in_max: number, out_min: number, out_max: number) {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
-    function motor_map(x: number)
-    {
-        x = x*16; // map 255 to 4096
-		if(x > 4095){
-			x= 4095;
-		}
-		if(x < -4095){
-			x= -4095;
-		}
+    function motor_map(x: number) {
+        x = x * 16; // map 255 to 4096
+        if (x > 4095) {
+            x = 4095;
+        }
+        if (x < -4095) {
+            x = -4095;
+        }
         return x;
     }
 
     //% blockId=KSR030_Frq_Set
     //% block="PWM Frequency Set %frqval"
     //% weight=99
-    export function Frq_Set(frqval:FrqState):void{
-       
-        if(!initialized){
-			init()
-		}
-        if(frqval==FrqState.A){
-            i2c_write(MODE1, 0x00);
-            // Constrain the frequency
-            setFreq(50*0.92);
+    export function Frq_Set(frqval: FrqState): void {
 
-        }else if(frqval==FrqState.B){
+        if (!initialized) {
+            init()
+        }
+        if (frqval == FrqState.A) {
             i2c_write(MODE1, 0x00);
             // Constrain the frequency
-            setFreq(50*0.94);
+            setFreq(50 * 0.92);
 
-        }else if(frqval==FrqState.C){
+        } else if (frqval == FrqState.B) {
             i2c_write(MODE1, 0x00);
             // Constrain the frequency
-            setFreq(50*0.96);
-            
-        }else if(frqval==FrqState.D){
+            setFreq(50 * 0.94);
+
+        } else if (frqval == FrqState.C) {
             i2c_write(MODE1, 0x00);
             // Constrain the frequency
-            setFreq(50*0.98);
-            
-        }else if(frqval==FrqState.E){
+            setFreq(50 * 0.96);
+
+        } else if (frqval == FrqState.D) {
             i2c_write(MODE1, 0x00);
             // Constrain the frequency
-            setFreq(50*1);
-            
-        }else if(frqval==FrqState.F){
+            setFreq(50 * 0.98);
+
+        } else if (frqval == FrqState.E) {
             i2c_write(MODE1, 0x00);
             // Constrain the frequency
-            setFreq(50*1.02);
-            
+            setFreq(50 * 1);
+
+        } else if (frqval == FrqState.F) {
+            i2c_write(MODE1, 0x00);
+            // Constrain the frequency
+            setFreq(50 * 1.02);
+
         }
     }
 
@@ -206,25 +230,25 @@ namespace KSR030 {
 
         const d = pins.pulseIn(DigitalPin.P13, PulseValue.High, maxCmDistance * 58);
         // read pulse
-        
+
         return Math.idiv(d, 58);
     }
 
-    
+
     //% blockId=KSR030_Track
     //% block="Track Sensor %sensor"
     //% weight=97
-    export function Read_Track(sensor:Track):number{
-        if(!initialized){
-			init()
-		}
-        if(sensor==Track.Left){
+    export function Read_Track(sensor: Track): number {
+        if (!initialized) {
+            init()
+        }
+        if (sensor == Track.Left) {
             return pins.digitalReadPin(DigitalPin.P12)
-        }else if(sensor==Track.Right){
+        } else if (sensor == Track.Right) {
             return pins.digitalReadPin(DigitalPin.P8)
-        }else{
+        } else {
             return -1
-        } 
+        }
     }
 
     //% blockId="KSR030_RGB" 
@@ -233,9 +257,9 @@ namespace KSR030 {
     export function RGB_LED(): neopixel.Strip {
         if (!neoStrip) {
             neoStrip = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
-           
+
         }
- 
+
         return neoStrip;
     }
 
@@ -248,12 +272,12 @@ namespace KSR030 {
     //% block="Servo channel %channel|degrees %degree"
     //% weight=86
     //% degree.min=0 degree.max=180
-	export function Servo(channel: ServoNum, degree: number): void {
-        
-        if(!initialized){
-			init()
-		}
-		// 50hz: 20,000 us
+    export function Servo(channel: ServoNum, degree: number): void {
+
+        if (!initialized) {
+            init()
+        }
+        // 50hz: 20,000 us
         //let servo_timing = (degree*1800/180+600) // 0.55 ~ 2.4
         //let pulselen = servo_timing*4096/20000
         //normal 0.5ms~2.4ms
@@ -262,9 +286,9 @@ namespace KSR030 {
         let pulselen = servo_map(degree, 0, 180, SERVOMIN, SERVOMAX);
         //let pulselen = servo_map(degree, 0, 180, servomin, servomax);
         setPwm(channel, 0, pulselen);
-  
+
     }
-    
+
 	/**
      * Used to move the given servo to the specified degrees (0-180) connected to the KSR030
      * @param channel The number (1-16) of the servo to move
@@ -275,12 +299,12 @@ namespace KSR030 {
     //% blockId=KSR030_ServoRange
     //% block="Servo channel %channel|degrees %degree|servomin %servomin|servomax %servomax"
     //% degree.min=0 degree.max=180
-	export function ServoRange(channel: ServoNum, degree: number, servomin: number, servomax: number): void {
-        
-        if(!initialized){
-			init()
-		}
-		// 50hz: 20,000 us
+    export function ServoRange(channel: ServoNum, degree: number, servomin: number, servomax: number): void {
+
+        if (!initialized) {
+            init()
+        }
+        // 50hz: 20,000 us
         //normal 0.5ms~2.4ms
         //SG90 0.5ms~2.0ms
         // servomin Servo_min_timing (ms)*1000*4096/20000 
@@ -293,25 +317,25 @@ namespace KSR030 {
 
     //% blockId=KSR030_Motor 
     //% block="Motor channel %channel|speed %speed"
-	//% weight=85
-	//% speed.min=-255 speed.max=255
-	export function Motor(channel: MotorNum, speed: number): void {
-		if(!initialized){
-			init()
+    //% weight=85
+    //% speed.min=-255 speed.max=255
+    export function Motor(channel: MotorNum, speed: number): void {
+        if (!initialized) {
+            init()
         }
-        
-        speed=motor_map(speed);
-        
-		let pwm1 = (channel*2)+12;
-		let pwm2 = (channel*2)+13;
-		if(speed>=0){
-			setPwm(pwm1, 0, speed)
-			setPwm(pwm2, 0, 0)
-		}else{
-			setPwm(pwm1, 0, 0)
-			setPwm(pwm2, 0, -speed)
+
+        speed = motor_map(speed);
+
+        let pwm1 = (channel * 2) + 12;
+        let pwm2 = (channel * 2) + 13;
+        if (speed >= 0) {
+            setPwm(pwm1, 0, speed)
+            setPwm(pwm2, 0, 0)
+        } else {
+            setPwm(pwm1, 0, 0)
+            setPwm(pwm2, 0, -speed)
         }
-            
+
     }
 
     //% blockId=KSR030_Servo_Car
@@ -319,44 +343,44 @@ namespace KSR030 {
     //% weight=88
     //% lspeed.min=-90 lspeed.max=90 rspeed.min=-90 rspeed.max=90
     export function Servo_Car(index: RunState, lspeed: number, rspeed: number): void {
-        let l_pulselen=0;
-        let r_pulselen=0;
-        if(!initialized){
-			init()
-		}
-        
+        let l_pulselen = 0;
+        let r_pulselen = 0;
+        if (!initialized) {
+            init()
+        }
+
         switch (index) {
-            case RunState.Forward: 
-                l_pulselen = servo_map(90+lspeed, 0, 180, SERVOMIN, SERVOMAX);
-                r_pulselen = servo_map(90-rspeed, 0, 180, SERVOMIN, SERVOMAX);
+            case RunState.Forward:
+                l_pulselen = servo_map(90 + lspeed, 0, 180, SERVOMIN, SERVOMAX);
+                r_pulselen = servo_map(90 - rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            case RunState.Back: 
-                l_pulselen = servo_map(90-lspeed, 0, 180, SERVOMIN, SERVOMAX);
-                r_pulselen = servo_map(90+rspeed, 0, 180, SERVOMIN, SERVOMAX);
+            case RunState.Back:
+                l_pulselen = servo_map(90 - lspeed, 0, 180, SERVOMIN, SERVOMAX);
+                r_pulselen = servo_map(90 + rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            case RunState.Left: 
-                l_pulselen = servo_map(90+lspeed, 0, 180, SERVOMIN, SERVOMAX);
-                r_pulselen = servo_map(90-rspeed, 0, 180, SERVOMIN, SERVOMAX);
+            case RunState.Left:
+                l_pulselen = servo_map(90 + lspeed, 0, 180, SERVOMIN, SERVOMAX);
+                r_pulselen = servo_map(90 - rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            case RunState.Right: 
-                l_pulselen = servo_map(90+lspeed, 0, 180, SERVOMIN, SERVOMAX);
-                r_pulselen = servo_map(90-rspeed, 0, 180, SERVOMIN, SERVOMAX);
+            case RunState.Right:
+                l_pulselen = servo_map(90 + lspeed, 0, 180, SERVOMIN, SERVOMAX);
+                r_pulselen = servo_map(90 - rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            case RunState.Stop: 
+            case RunState.Stop:
                 l_pulselen = servo_map(90, 0, 180, SERVOMIN, SERVOMAX);
                 r_pulselen = servo_map(90, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            
+
         }
     }
 
@@ -366,46 +390,46 @@ namespace KSR030 {
     //% lspeed.min=-255 lspeed.max=255 rspeed.min=-255 rspeed.max=255
     export function Motor_Car(index: RunState, lspeed: number, rspeed: number): void {
         switch (index) {
-            case RunState.Forward: 
-                Motor(MotorNum.M1B,lspeed);
-                Motor(MotorNum.M1A,rspeed);
+            case RunState.Forward:
+                Motor(MotorNum.M1B, lspeed);
+                Motor(MotorNum.M1A, rspeed);
                 break;
-            case RunState.Back: 
-                Motor(MotorNum.M1B,-lspeed);
-                Motor(MotorNum.M1A,-rspeed);
+            case RunState.Back:
+                Motor(MotorNum.M1B, -lspeed);
+                Motor(MotorNum.M1A, -rspeed);
                 break;
-            case RunState.Left: 
-                Motor(MotorNum.M1B,lspeed);
-                Motor(MotorNum.M1A,rspeed);
+            case RunState.Left:
+                Motor(MotorNum.M1B, lspeed);
+                Motor(MotorNum.M1A, rspeed);
                 break;
-            case RunState.Right: 
-                Motor(MotorNum.M1B,lspeed);
-                Motor(MotorNum.M1A,rspeed); 
+            case RunState.Right:
+                Motor(MotorNum.M1B, lspeed);
+                Motor(MotorNum.M1A, rspeed);
                 break;
-            case RunState.Stop: 
-                Motor(MotorNum.M1B,0);
-                Motor(MotorNum.M1A,0);
+            case RunState.Stop:
+                Motor(MotorNum.M1B, 0);
+                Motor(MotorNum.M1A, 0);
                 break;
-            
+
         }
     }
 
     //% blockId=PWM_DETECT_Frequency
     //% block="DETECT Servo %channel Frequency to pin %iopin"
     //% weight=80
-    export function DETECT_Frequency(channel: ServoNum, iopin: DigitalPin): number  {
+    export function DETECT_Frequency(channel: ServoNum, iopin: DigitalPin): number {
         let frq = 0;
         let frqPinState = 0;
         let prevFrqPinState = 0;
         let oneSecond = 1000;
         let timer = 0;
         let ret_frq = 0;
-       
-        if(!initialized){
-			init()
+
+        if (!initialized) {
+            init()
         }
         setPwm(channel, 0, SERVOMAX);
-        for(let i=0; i<2000 ; i++) {
+        for (let i = 0; i < 2000; i++) {
             frqPinState = pins.digitalReadPin(iopin)
             if (frqPinState == 0) {
                 prevFrqPinState = 0
@@ -417,38 +441,38 @@ namespace KSR030 {
             control.waitMicros(1000)
             timer = timer + 1
             if (timer > oneSecond) {
-                frq = frq-2
+                frq = frq - 2
                 if (frq > 53) {
                     //basic.showString("A")
-                    ret_frq= 65
+                    ret_frq = 65
                 } else {
                     if (frq > 52) {
                         //basic.showString("B")
-                        ret_frq= 66
+                        ret_frq = 66
                     } else {
                         if (frq > 51) {
                             //basic.showString("C")
-                            ret_frq= 67
+                            ret_frq = 67
                         } else {
                             if (frq > 50) {
                                 //basic.showString("D")
-                                ret_frq=  68
+                                ret_frq = 68
                             } else {
                                 if (frq > 49) {
                                     //basic.showString("E")
-                                    ret_frq=  69
+                                    ret_frq = 69
                                 } else {
                                     if (frq > 48) {
                                         //basic.showString("F")
-                                        ret_frq=  70
+                                        ret_frq = 70
                                     } else {
-                                        if(frq <= 48) {
+                                        if (frq <= 48) {
                                             //basic.showString("X")
-                                            ret_frq=  88
+                                            ret_frq = 88
 
                                         }
                                     }
-                                        
+
                                 }
                             }
                         }
@@ -460,7 +484,7 @@ namespace KSR030 {
             }
         }
         return ret_frq
-        
+
 
     }
 
