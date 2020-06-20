7536b28eeb4a828f283163ec666a5224ced325b1
arthur27222764 <48074375+arthur27222764@users.noreply.github.com>
2020-06-20 13:02:44 +0800

    v0.6.6

--- main.ts
+++ main.ts
@@ -436,13 +436,13 @@ namespace KSR030 {
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            /*case RunState.Left:
+            case RunState.Left:
                 l_pulselen = servo_map(90 + lspeed, 0, 180, SERVOMIN, SERVOMAX);
                 r_pulselen = servo_map(90 - rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
                 setPwm(ServoNum.S9, 0, r_pulselen);
                 break;
-            case RunState.Right:
+            /*case RunState.Right:
                 l_pulselen = servo_map(90 + lspeed, 0, 180, SERVOMIN, SERVOMAX);
                 r_pulselen = servo_map(90 - rspeed, 0, 180, SERVOMIN, SERVOMAX);
                 setPwm(ServoNum.S8, 0, l_pulselen);
