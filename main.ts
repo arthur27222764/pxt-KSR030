180de7ee5dcbd9eb0ab6bf48cf3322bc39c1745a
arthur27222764 <48074375+arthur27222764@users.noreply.github.com>
2020-06-21 12:17:15 +0800

    0.9.7

--- main.ts
+++ main.ts
@@ -103,7 +103,7 @@ namespace KSR030 {
 
         detect_freq(ServoNum.S0, DigitalPin.P2, 1);
         detect_freq(ServoNum.S0, DigitalPin.P2, 1);
-        //i2c_write(MODE1, 0x00);
+        i2c_write(MODE1, 0x00);
         //setFreq(50);
 
         initialized = true;
