3894c0f6accb3bb1b42da27bc470e96124959234
kaise-ksrobot <arthur@kaise.com.tw>
2020-06-30 00:21:57 +0800

    v0.7.9

--- main.ts
+++ main.ts
@@ -101,10 +101,7 @@ namespace KSR030 {
         pins.setPull(DigitalPin.P8, PinPullMode.PullUp);
         pins.setPull(DigitalPin.P12, PinPullMode.PullUp);
         i2c_setFreq(50);
-        control.waitMicros(30000)
-        i2c_setFreq(50);
-        control.waitMicros(30000)
-        //servo_pwm(detect_freq(ServoNum.S0, DigitalPin.P2));
+        servo_pwm(detect_freq(ServoNum.S0, DigitalPin.P2));
 
 
         initialized = true;
