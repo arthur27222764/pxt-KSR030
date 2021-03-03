95d18aff1fbaafec442f93bfe61e6065c833fe1e
arthur <arthur@kaise.com.tw>
2021-03-03 12:23:19 +0800

    v1.2.4

--- main.ts
+++ main.ts
@@ -2,6 +2,16 @@
  * KSR030 V0.010
  */
 
+declare namespace kspins{
+    /**
+     * Get the duration of the last pulse in microseconds. This function should be called from a ``onPulsed`` handler.
+     */
+    //% advanced=true
+    //% blockId=kspins_pulse_duration block="pulse duration (µs)"
+    //% weight=21 blockGap=8 shim=kspins::pulseDuration
+    function pulseDuration(): int32;
+ 
+}
 
 
 
