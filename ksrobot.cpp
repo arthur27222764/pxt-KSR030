cfc05eb8f21f4363569c78f7b75bdd04e5281c61
arthur <arthur@kaise.com.tw>
2021-03-05 01:04:13 +0800

    v1.3.4

--- ksrobot.cpp
+++ ksrobot.cpp
@@ -1,9 +1,14 @@
 #include "pxt.h"
 
-namespace ksrobotlib
+namespace input
 {
 
-    //%
+    /**
+    * Reads the loudness through the microphone from 0 (silent) to 255 (loud)
+    */
+    //% blockId=device_get_mb_version block="mb_version"
+    //% weight=34 blockGap=8
+    //% group="micro:bit (V2)"
     int mb_version()
     {
 #if MICROBIT_CODAL
--- shims.d.ts
+++ shims.d.ts
@@ -1,8 +1,13 @@
-//% color=#E3008C weight=96 icon="\uf012"
-declare namespace ksrobotlib {
 
-  //% group="micro:bit (V2)" shim=ksrobotlib::mb_version
-  function mb_version(): int32;
+declare namespace input {
+
     
+    /**
+    * Reads the loudness through the microphone from 0 (silent) to 255 (loud)
+    */
+    //% blockId=device_get_mb_version block="mb_version"
+    //% weight=34 blockGap=8
+    //% group="micro:bit (V2)" shim=input::mb_version
+    function mb_version(): int32;
 
 }
